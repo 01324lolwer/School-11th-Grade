61b54325afbee55f876e8a252dfaaa0538454ad6
Lol Wer / JJ <154529150+01324lolwer@users.noreply.github.com>
2024-06-04 13:00:49 +0200

    Funktionales Main menu
    Funktioniert

--- Jump'n'Run/Jump'n'Run.gd
+++ Jump'n'Run/Jump'n'Run.gd
@@ -10,3 +10,7 @@ func _on_Void_body_entered(body):
 func _on_Ziel_body_entered(body):
 	if body is Player:
 		get_tree().reload_current_scene()
+
+
+func _on_CollisionShape2D_child_entered_tree(node):
+		get_tree().reload_current_scene()
--- Jump'n'Run/MainMenu.gd
+++ Jump'n'Run/MainMenu.gd
@@ -3,4 +3,10 @@ extends MarginContainer
 
 
 func _on_Button_pressed():
+	get_tree().change_scene("res://Jump'n'Run.tscn")
+	pass # Replace with function body.
+
+
+func _on_Button2_pressed():
+	get_tree().quit()
 	pass # Replace with function body.
