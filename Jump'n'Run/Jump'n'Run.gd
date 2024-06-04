2d1df93642376b0686f87884a42292f9c6de4f8a
Lol Wer / JJ <154529150+01324lolwer@users.noreply.github.com>
2024-06-04 11:49:20 +0200

    Save with main menu
    Base game and main menu.

---
+++ Jump'n'Run/Jump'n'Run.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+
+func _on_Void_body_entered(body):
+	if body is Player:
+		get_tree().reload_current_scene()
+
+
+func _on_Ziel_body_entered(body):
+	if body is Player:
+		get_tree().reload_current_scene()
