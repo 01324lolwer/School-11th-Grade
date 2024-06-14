b81c1fb24bbfbb94a007007f533b77e3ab1bc915
Lol Wer / JJ <154529150+01324lolwer@users.noreply.github.com>
2024-06-14 20:46:46 +0200

    Lvl 3 zu main menu
    after the second level u get put in the main menu because the 3rd lvl doesn't exist yet.

--- Jump'n'Run/Level 3.gd
+++ Jump'n'Run/Level 3.gd
@@ -4,7 +4,7 @@ extends Node2D
 
 func _on_Ziel_body_entered(body):
 	if body is Player:
-		get_tree().change_scene("res://Level 3.tscn")
+		get_tree().change_scene("res://MainMenu.tscn")
 
 
 func _on_Void_body_entered(body):
